6ed043db35a34f81b3d543784dd0e63b42c8e21e
tigercactus <tigercactus@yandex.ru>
2023-09-03 15:20:39 +0300

    second commit

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,9 +1,33 @@
-public class Bicycle {
+public class Bicycle implements Service {
 
-    public String modelName;
-    public int wheelsCount;
+    private String modelName;
+    private int wheelsCount;
 
-    public void updateTyre() {
+    public void introduceMe(){
+        System.out.println("Обслуживаем " + getModelName());
+    }
+    public void serveMe() {
         System.out.println("Меняем покрышку");
+    }
+
+    public Bicycle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
     }
 }
